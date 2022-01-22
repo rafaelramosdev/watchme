c9014db1249da2874818a019e7e22a1b9cdf643e
Rafael Ramos <rafaelramosdev@gmail.com>
2022-01-22 16:35:54 -0300

    feat: created header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import '../styles/header.scss'
+
+interface HeaderProps {
+  selectedGenreTitle: string
+}
+
+export function Header(props: HeaderProps) {
+  return (
+    <header>
+      <span className="category">Categoria:<span> {props.selectedGenreTitle}</span></span>
+    </header>
+  )
+}
